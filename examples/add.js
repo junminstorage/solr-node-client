beac9af654fa22d251f3fd6c304653c752ff4576
Junmin Liu <jliu670@bloomberg.net>
2015-08-28 17:49:43 -0400

    add many changes

--- examples/add.js
+++ examples/add.js
@@ -3,16 +3,21 @@
  */
 
 // Use `var solr = require('solr-client')` in your code 
-var solr = require('./../lib/solr'); 
+var solr = require('./../lib/solr');
+var httpProxy = require('http-proxy-agent');
+
+var proxy = process.env.http_proxy || 'http://bproxy.cfe.bloomberg.com:80';
+console.log('use proxy server %j', proxy);
+var agent = new httpProxy(proxy);
 
 // Create a client
-var client = solr.createClient();
+var client = solr.createClient({host:'sundev29.dev.bloomberg.com', core:'test', agent:agent});
 
 // Switch on "auto commit", by default `client.autoCommit = false`
 client.autoCommit = true;
 
 var docs = [];
-for(var i = 0; i <= 10 ; i++){
+for(var i = 0; i <= 1 ; i++){
    var doc = {
        id : 12345 + i,
        title_t : "Title "+ i,
